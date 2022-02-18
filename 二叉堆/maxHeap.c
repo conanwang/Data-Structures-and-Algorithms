b3f30e7dead7515d8656939e18b9604570011440
conanwang <26738256@qq.com>
2022-02-18 18:15:03 +0800

    第一次提交

---
+++ 二叉堆/maxHeap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// 堆的容量
+#define MAX_SIZE 10
+
+// 定义最大堆结构体，这里我们的第一个元素从索引1开始，出于方便
+typedef struct maxHeap {
+  int size;             // 堆的容量
+  int data[MAX_SIZE];   // 堆的底层数组   
+} heap;
+
+
+int main(void) {
+  
+}
